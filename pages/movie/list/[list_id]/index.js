0f8f5cb3a8e0264fa92db9f006e1ff163af459e6
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-10-10 01:27:09 +0200

    Add images alternative

--- pages/movie/list/[list_id]/index.js
+++ pages/movie/list/[list_id]/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { TbMovie } from "react-icons/tb";
 import Link from "next/link";
+import Img from './../../../../components/Img';
 const Index = (props) => {
   const data = props.data.results;
   return (
@@ -18,10 +19,11 @@ const Index = (props) => {
             <Link href={`/movie/${movie.id}`}>
               <div>
                 <div>
-                  <img
+                  {/* <img
                     className="rounded-lg"
                     src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-                  />
+                  /> */}
+                  <Img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} className={"rounded-lg"}/>
                 </div>
                 <div className="sm:absolute bottom-3 left-3 bg-white p-3 rounded-lg bg-opacity-90 z-10 flex gap-3 mt-2 content-center justify-items-end">
                   <TbMovie size={25} />
