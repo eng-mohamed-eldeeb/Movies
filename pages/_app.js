634208430143dd9adeadde3ea23762c71aa04c5f
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-10-12 14:36:44 +0200

    Add the favicon

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,21 @@ import "../styles/index.css";
 import SearchBar from "./../components/searchBar";
 import NavBar from "./../components/NavBar";
 import Footer from "../components/Footer";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="bg-slate-900 min-h-screen">
-      <NavBar />
-      <SearchBar />
-      <Component {...pageProps} />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Movies land</title>
+        <link rel="icon" type="image/png" href="/videoplayer.png"></link>
+      </Head>
+      <div className="bg-slate-900 min-h-screen">
+        <NavBar />
+        <SearchBar />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </>
   );
 }
 
